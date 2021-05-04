23dc0e044ef8bf6dc293bfca587ba0008f4cefbe
AbhiroopKar-p5Creator <clashandbrawlgaming2020@gmail.com>
2021-05-04 09:01:03 -0700

    Day-2

---
+++ Bos-2/Assets/Scripts/Lean.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lean : MonoBehaviour
+{
+
+    public float _leanX;
+    public float _leanY;
+    
+    
+    
+  private void Update()
+  {
+      
+
+      if (Input.GetKey(KeyCode.P) && Input.GetKey(KeyCode.X))
+      {
+          _leanX += 1f;  
+      } else if (Input.GetKey(KeyCode.M) && Input.GetKey(KeyCode.X))
+      {
+          _leanX -= 1f;
+      } else if (Input.GetKey(KeyCode.P) && Input.GetKey(KeyCode.Y))
+      {
+          _leanY += 1f;  
+      } else if (Input.GetKey(KeyCode.M) && Input.GetKey(KeyCode.Y))
+      {
+          _leanY -= 1f;
+      }
+       
+      Mathf.Clamp(_leanX, -90, 90);
+      Mathf.Clamp(_leanY, -90, 90);
+      this.transform.localRotation = Quaternion.Euler(0f, _leanX, _leanY);
+     
+  }
+}
