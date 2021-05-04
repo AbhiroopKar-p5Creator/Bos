803a1e5fb1228d85811ee96f81f1133338996aec
AbhiroopKar-p5Creator <clashandbrawlgaming2020@gmail.com>
2021-05-04 09:52:06 -0700

    LasetChange-Day2

--- Bos-2/Assets/Scripts/Lean.cs
+++ Bos-2/Assets/Scripts/Lean.cs
@@ -1,16 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Lean : MonoBehaviour
 {
 
-    public float _leanX;
-    public float _leanY;
+    private float _leanX;
+    private float _weightMatrix;
     
-    
-    
-  private void Update()
+
+
+    private void Update()
   {
       
 
@@ -20,17 +18,13 @@ public class Lean : MonoBehaviour
       } else if (Input.GetKey(KeyCode.M) && Input.GetKey(KeyCode.X))
       {
           _leanX -= 1f;
-      } else if (Input.GetKey(KeyCode.P) && Input.GetKey(KeyCode.Y))
-      {
-          _leanY += 1f;  
-      } else if (Input.GetKey(KeyCode.M) && Input.GetKey(KeyCode.Y))
-      {
-          _leanY -= 1f;
       }
+
+      _weightMatrix = 0f; // temp
        
       Mathf.Clamp(_leanX, -90, 90);
-      Mathf.Clamp(_leanY, -90, 90);
-      this.transform.localRotation = Quaternion.Euler(0f, _leanX, _leanY);
-     
+      Mathf.Clamp(_weightMatrix, -1f, 1f);
+      this.transform.localRotation = Quaternion.Euler(0f, _leanX, 0f);
+
   }
 }
